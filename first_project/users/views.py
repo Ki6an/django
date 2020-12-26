46e2a4dd2834aadcb9acb04565632d9727de09c1
KIRAN <kiranr8k@gmail.com>
2020-12-26 20:50:21 +0530

    added update option in profile

--- first_project/users/views.py
+++ first_project/users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -14,13 +15,36 @@ def register(request):
             messages.success(
                 request,
                 f'Your account has been created, You are now able to login!'
-                            )
+            )
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-    
+
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        P_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+
+        if u_form.is_valid() and P_form.is_valid():
+            u_form.save()
+            P_form.save()
+            messages.success(
+                request,
+                f'Your account has been updated!'
+            )
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        P_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form':  P_form
+    }
+    return render(request, 'users/profile.html', context)
