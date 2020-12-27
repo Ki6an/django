946120873d500c729553b8a9380d87e9a79c7901
KIRAN <kiranr8k@gmail.com>
2020-12-27 09:29:28 +0530

    update post

--- first_project/blog/urls.py
+++ first_project/blog/urls.py
@@ -5,12 +5,15 @@ from . import views
 from .views import (
     PostListView,
     PostDetailView,
-    PostCreateView
+    PostCreateView,
+    PostUpdateView
 )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),  # views.home
     path('about/', views.about, name='blog-about'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  
-    path('post/new/', PostCreateView.as_view(), name='post-create')
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
 ]
